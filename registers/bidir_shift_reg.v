da91aa7e5f0718ace1e44c2e514171e9e0aab6e0
sanjay <118981880+realbeliver@users.noreply.github.com>
2026-02-10 16:24:36 +0530

    Add bidir_shift_reg module in registers folder

---
+++ registers/bidir_shift_reg.v
@@ -0,0 +1,17 @@
+timescale 1ns / 1ps
+module bidir_shift_reg (
+    input clk,
+    input rst,
+    input dir,
+    input si,
+    output reg [3:0] q
+);
+    always @(posedge clk) begin
+        if (rst)
+            q <= 4'b0000;
+        else if (dir)
+            q <= {q[2:0], si};
+        else
+            q <= {si, q[3:1]};
+    end
+endmodule
